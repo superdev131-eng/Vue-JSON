fcce4607cac5bef54906eb8b84ebb9328cc0df83
Jkverma1 <jatins9227@gmail.com>
2023-01-04 19:33:03 +0530

    sidebar completed

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -23,29 +23,111 @@
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
 
 <div class="dropdown-nav-item">
-  <button class="dropdown-btn btn" id="btn">
+  <button class="btn dropdown-btn" id="btn">
     Form Menu
     <i class="fa-solid fa-chevron-down drop-icon" id="arrow"></i>
   </button>
 
   <div class="dropdown" id="dropdown">
-   <div class="dropdown-nav-item">
-    <a href="#">Nav Item</a>
-   </div>
-   <div class="dropdown-nav-item">
-    <a href="#">Nav Item</a>
-   </div>
-   <div class="dropdown-nav-item">
-    <a href="#">Nav Item</a>
-   </div>
-   <div class="dropdown-nav-item">
-    <a href="#">Nav Item</a>
-   </div>
+    <div class="sub-dropdown-nav-item">
+  <button class="sub-btn sub-dropdown-btn" id="sub-btn1">
+    CPD
+    <i class="fa-solid fa-chevron-down drop-icon" id="sub-arrow1"></i>
+  </button>
+
+  <div class="dropdown sub-dropdown" id="sub-dropdown1">
+    <div class="dropdown-nav">
+    <div class="sub-dropdown-nav-item">
+        <button class="sub-btn sub-dropdown-btn" id="sub-sub-btn1">
+            Basics
+            <i class="fa-solid fa-chevron-down drop-icon" id="sub-sub-arrow1"></i>
+        </button>
+
+    <div class="dropdown" id="sub-sub-dropdown1">
+        <div>
+            <a href="#" class="sub-drop-nav-link">Sample</a>
+        </div>
+    </div>
+    </div>
+</div>
+<div class="dropdown-nav">
+<div class="sub-dropdown-nav-item">
+        <button class="sub-btn sub-dropdown-btn" id="sub-sub-btn2">
+            Crew Data
+            <i class="fa-solid fa-chevron-down drop-icon" id="sub-sub-arrow2"></i>
+        </button>
+
+        <div class="dropdown" id="sub-sub-dropdown2">
+            <div>
+                <a href="#" class="sub-drop-nav-link">Sample</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="dropdown-nav">
+    <div class="sub-dropdown-nav-item">
+        <button class="sub-btn sub-dropdown-btn" id="sub-sub-btn3">
+            Form for Crew
+            <i class="fa-solid fa-chevron-down drop-icon" id="sub-sub-arrow3"></i>
+        </button>
+
+        <div class="dropdown" id="sub-sub-dropdown3">
+            <div>
+                <a href="#" class="sub-drop-nav-link">Voyage Bonus</a>
+                <a href="#" class="sub-drop-nav-link">Cash To Master</a>
+                <a href="#" class="sub-drop-nav-link">Hand Over Report</a>
+                <a href="#" class="sub-drop-nav-link">Familiar List</a>
+                <a href="#" class="sub-drop-nav-link">G-15 Sickness <br> Injury Report</a>
+            </div>
+        </div>
+    </div>
+</div>
   </div>
 </div>
-<div class="nav-item">
-            <a href="#" class="nav-link">Vessel</a>
-        </div>
+<div class="sub-dropdown-nav-item">
+    <button class="sub-btn sub-dropdown-btn" id="sub-btn2">
+      BD
+      <i class="fa-solid fa-chevron-down drop-icon" id="sub-arrow2"></i>
+    </button>
+  
+    <div class="dropdown sub-dropdown" id="sub-dropdown2">
+     
+    </div>
+  </div>
+  
+  <div class="sub-dropdown-nav-item">
+    <button class="sub-btn sub-dropdown-btn" id="sub-btn3">
+      ISM
+      <i class="fa-solid fa-chevron-down drop-icon" id="sub-arrow3"></i>
+    </button>
+  
+    <div class="dropdown sub-dropdown" id="sub-dropdown3">
+     
+    </div>
+  </div>
+  <div class="sub-dropdown-nav-item">
+    <button class="sub-btn sub-dropdown-btn" id="sub-btn4">
+      PD
+      <i class="fa-solid fa-chevron-down drop-icon" id="sub-arrow4"></i>
+    </button>
+  
+    <div class="dropdown sub-dropdown" id="sub-dropdown4">
+     
+    </div>
+  </div>
+  <div class="sub-dropdown-nav-item">
+    <button class="sub-btn sub-dropdown-btn" id="sub-btn5">
+      ESD
+      <i class="fa-solid fa-chevron-down drop-icon" id="sub-arrow5"></i>
+    </button>
+  
+    <div class="dropdown sub-dropdown" id="sub-dropdown5">
+     
+    </div>
+  </div>
+  </div>
+</div>
+
     </nav>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -24,9 +24,153 @@ dropdownBtn.addEventListener("click", function (e) {
   toggleDropdown();
 });
 
-// Close dropdown when dom element is clicked
-document.documentElement.addEventListener("click", function () {
-  if (dropdownMenu.classList.contains("show")) {
-    toggleDropdown();
-  }
-}); 
+
+const dropdownBtn1 = document.getElementById("sub-btn1");
+const dropdownMenu1 = document.getElementById("sub-dropdown1");
+const toggleArrow1 = document.getElementById("sub-arrow1");
+
+// Toggle dropdown function
+const toggleDropdown1 = function () {
+  dropdownMenu1.classList.toggle("show");
+  dropdownBtn1.classList.toggle("font-700");
+  toggleArrow1.classList.toggle("arrow");
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn1.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  toggleDropdown1();
+});
+
+
+const dropdownBtn2 = document.getElementById("sub-btn2");
+const dropdownMenu2 = document.getElementById("sub-dropdown2");
+const toggleArrow2 = document.getElementById("sub-arrow2");
+
+// Toggle dropdown function
+const toggleDropdown2 = function () {
+  dropdownMenu2.classList.toggle("show");
+  dropdownBtn2.classList.toggle("font-700");
+  toggleArrow2.classList.toggle("arrow");
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn2.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  toggleDropdown2();
+});
+
+const dropdownBtn3 = document.getElementById("sub-btn3");
+const dropdownMenu3 = document.getElementById("sub-dropdown3");
+const toggleArrow3 = document.getElementById("sub-arrow3");
+
+// Toggle dropdown function
+const toggleDropdown3 = function () {
+  dropdownMenu3.classList.toggle("show");
+  dropdownBtn3.classList.toggle("font-700");
+  toggleArrow3.classList.toggle("arrow");
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn3.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  toggleDropdown3();
+});
+
+
+const dropdownBtn4 = document.getElementById("sub-btn4");
+const dropdownMenu4 = document.getElementById("sub-dropdown4");
+const toggleArrow4 = document.getElementById("sub-arrow4");
+
+// Toggle dropdown function
+const toggleDropdown4 = function () {
+  dropdownMenu4.classList.toggle("show");
+  dropdownBtn4.classList.toggle("font-700");
+  toggleArrow4.classList.toggle("arrow");
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn4.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  toggleDropdown4();
+});
+
+
+const dropdownBtn5 = document.getElementById("sub-btn5");
+const dropdownMenu5 = document.getElementById("sub-dropdown5");
+const toggleArrow5 = document.getElementById("sub-arrow5");
+
+// Toggle dropdown function
+const toggleDropdown5 = function () {
+  dropdownMenu5.classList.toggle("show");
+  dropdownBtn5.classList.toggle("font-700");
+  toggleArrow5.classList.toggle("arrow");
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownBtn5.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  toggleDropdown5();
+});
+
+const subdropdownBtn1 = document.getElementById("sub-sub-btn1");
+const subdropdownMenu1 = document.getElementById("sub-sub-dropdown1");
+const subtoggleArrow1 = document.getElementById("sub-sub-arrow1");
+
+// Toggle dropdown function
+const subtoggleDropdown1 = function () {
+  subdropdownMenu1.classList.toggle("show");
+  subdropdownBtn1.classList.toggle("font-700");
+  subtoggleArrow1.classList.toggle("arrow");
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+subdropdownBtn1.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  subtoggleDropdown1();
+});
+
+
+const subdropdownBtn2 = document.getElementById("sub-sub-btn2");
+const subdropdownMenu2 = document.getElementById("sub-sub-dropdown2");
+const subtoggleArrow2 = document.getElementById("sub-sub-arrow2");
+
+// Toggle dropdown function
+const subtoggleDropdown2 = function () {
+  subdropdownMenu2.classList.toggle("show");
+  subdropdownBtn2.classList.toggle("font-700");
+  subtoggleArrow2.classList.toggle("arrow");
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+subdropdownBtn2.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  subtoggleDropdown2();
+});
+
+const subdropdownBtn3 = document.getElementById("sub-sub-btn3");
+const subdropdownMenu3 = document.getElementById("sub-sub-dropdown3");
+const subtoggleArrow3 = document.getElementById("sub-sub-arrow3");
+
+// Toggle dropdown function
+const subtoggleDropdown3 = function () {
+  subdropdownMenu3.classList.toggle("show");
+  subdropdownBtn3.classList.toggle("font-700");
+  subtoggleArrow3.classList.toggle("arrow");
+  
+};
+// font-700
+// Toggle dropdown open/close when dropdown button is clicked
+subdropdownBtn3.addEventListener("click", function (e) {
+  e.stopPropagation();
+
+  subtoggleDropdown3();
+});
+
