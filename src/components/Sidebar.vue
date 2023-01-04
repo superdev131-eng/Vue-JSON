423814c45e3e1524d5523c4d44abd0d16e6cbe7b
divyasoni9037 <Jkverma1>
2023-01-04 13:58:49 +0530

    sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,49 +1,53 @@
 <script>
+
 </script>
 
 <template>
-  <div class='dashboard'>
-    <div class="dashboard-nav">
-        <header><a href="#!" class="menu-toggle fa-solid fa-bars"><i class="fas fa-bars"></i></a><a href="#"
-                                                                                   class="brand-logo">
-                                                                                <img src="../assets/img/logo.png" alt=""></a></header>
-        <nav class="dashboard-nav-list"><a href="#" class="dashboard-nav-item">
-            Home </a><a
-                href="#" class="dashboard-nav-item active"> dashboard
-        </a><a
-                href="#" class="dashboard-nav-item"> Company </a>
-            <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"> Vessel </a>
-                <div class='dashboard-nav-dropdown-menu'><a href="#"
-                                                            class="dashboard-nav-dropdown-item">All</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Recent</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Images</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Video</a></div>
-            </div>
-            <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"> Form Menu </a>
-                <div class='dashboard-nav-dropdown-menu'><a href="#"
-                                                            class="dashboard-nav-dropdown-item">All</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Subscribed</a><a
-                        href="#"
-                        class="dashboard-nav-dropdown-item">Non-subscribed</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Banned</a><a
-                        href="#" class="dashboard-nav-dropdown-item">New</a></div>
-            </div>
-            <div class='dashboard-nav-dropdown'><a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"> BD </a>
-                <div class='dashboard-nav-dropdown-menu'><a href="#"
-                                                            class="dashboard-nav-dropdown-item">All</a><a
-                        href="#" class="dashboard-nav-dropdown-item">Recent</a><a
-                        href="#" class="dashboard-nav-dropdown-item"> Projections</a>
-                </div>
-            </div>
-            <a
-                    href="#" class="dashboard-nav-item"> ISM </a>
-          <a
-                    href="#" class="dashboard-nav-item"> PD </a>
-                    <a
-                    href="#" class="dashboard-nav-item"> ESD </a>
-        </nav>
+  <div class="sidebar">
+    <div class="sidebar-brand">
+        <img src="../assets/img/logo.png" class="siebar-title" alt="logo">
     </div>
+    <nav class="sidebar-nav">
+        <div class="nav-item">
+            <a href="#" class="nav-link">Home</a>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="nav-link">Dashboard</a>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="nav-link">Company</a>
+        </div>
+        <div class="nav-item">
+            <a href="#" class="nav-link">Vessel</a>
+        </div>
+        <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+
+<div class="dropdown-nav-item">
+  <button class="dropdown-btn btn" id="btn">
+    Form Menu
+    <i class="fa-solid fa-chevron-down drop-icon" id="arrow"></i>
+  </button>
+
+  <div class="dropdown" id="dropdown">
+   <div class="dropdown-nav-item">
+    <a href="#">Nav Item</a>
+   </div>
+   <div class="dropdown-nav-item">
+    <a href="#">Nav Item</a>
+   </div>
+   <div class="dropdown-nav-item">
+    <a href="#">Nav Item</a>
+   </div>
+   <div class="dropdown-nav-item">
+    <a href="#">Nav Item</a>
+   </div>
+  </div>
 </div>
+<div class="nav-item">
+            <a href="#" class="nav-link">Vessel</a>
+        </div>
+    </nav>
+  </div>
 </template>
 
 <style></style>
